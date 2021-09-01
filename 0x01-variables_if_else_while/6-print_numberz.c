7b2fc58e041ba17d12ee7d97523833e5681214cf
Dagmawi <Dagimanalex27@gmail.com>
2021-09-01 12:52:38 -0700

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,8 @@
-  
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: Print all numbers of base 10, starting from 0.
+ * Return: 0
  */
 int main(void)
 {
@@ -12,8 +10,8 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(i + 48);
+		putchar(i + '0');
 	}
-	putchar(10);
+	putchar('\n');
 	return (0);
 }
