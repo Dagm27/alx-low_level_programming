81c837fba68d967278e92adf60fea56892a3867e
Dagmawi <Dagimanalex27@gmail.com>
2021-09-20 06:10:27 -0700

    1digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *_isdigit - check for digit
+  *@x: digit to be checked 
+  *
+  *return: 1 for digit 0 for other 
+  */
+int _isdigit(int x)
+{
+	if (x >= 48 && x <=57)
+	{
+		return (1);
+	}	
+         return (0);
+}	 
+
