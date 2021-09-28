516cab93cb3dc9c4e7bca39365e3fc2d9e525976
Dagmawi <Dagimanalex27@gmail.com>
2021-09-28 14:32:00 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
   *main - entry point
   *
-  *Return - always 0
+  *Return:  always 0
   */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
