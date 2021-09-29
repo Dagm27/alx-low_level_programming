4eb668d1cebaec5e496a0cb88d9aa6048a48de26
Dagmawi <Dagimanalex27@gmail.com>
2021-09-29 15:34:00 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *main - print numbers
+  *
+  *Return: always 0
+  */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	for (a = 0; a < 11; a++)
+		printf("%d\n", a);
+	return (0);
+}
