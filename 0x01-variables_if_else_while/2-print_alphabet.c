501d242a0f85bb352c602d94e2d1e4e73136b096
Dagmawi <Dagimanalex27@gmail.com>
2021-09-29 14:58:28 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - prints alphabet lower case
+  *@ch - alphabet
+  *
+  *Return: always 0 (success)
+  */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+	return (0);
+}
