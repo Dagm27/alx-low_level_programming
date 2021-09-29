8ae15f4acba2419344fc0c9fde65b5efa3259b3b
Dagmawi <Dagimanalex27@gmail.com>
2021-09-29 08:22:46 -0700

    first

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,7 +18,7 @@ int main(void)
 		printf("%d is negative\n", n);
 	} else if (n > 0)
 	{
-		printf("%d is posititve\n", n);
+		printf("%d is positive\n", n);
 	} else
 	{
 		printf("%d is zero\n", n);
