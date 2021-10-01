3513a88654939cb83256f6e4be6be08c1dd36a59
Dagmawi <Dagimanalex27@gmail.com>
2021-10-01 14:51:10 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  *_abs - prints the absolute value
+  *@c: number
+  *
+  *Return: n
+  */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		n = n;
+	} else if (n < 0)
+	{
+		n = n - (n + n);
+	}
+	else
+	{
+		n = 0;
+	}
+	return (n);
+}
