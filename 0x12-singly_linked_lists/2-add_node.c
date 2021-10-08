cdb7e020bf5952309c7fbd76d3be242c8d9165ad
Dagmawi <Dagimanalex27@gmail.com>
2021-10-08 14:21:39 -0700

    addnode

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,37 +1,51 @@
+#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
-#include "lists.h"
+
 /**
- * add_node - Adds a new node at the beginning of a list
- * @head: The original linked list
- * @str: The string to add to the node
- *
- * Return: The address of the new list or NULL if it failed
+ * *add_node - adds node to the beginning of list_t
+ * @head: first node
+ * @str: pointer to list
+ * Return: new node(success) otherwise null
  */
 list_t *add_node(list_t **head, const char *str)
-{ list_t *temp;
-if (head != NULL && str != NULL)
 {
-temp = malloc(sizeof(list_t));
-if (temp == NULL)
-return (NULL);
-temp->str = strdup(str);
-temp->len = _strlen(str);
-temp->next = *head;
-*head = temp;
-return (temp); }
-return (0); }
+	list_t *temp;
+
+	if (str == NULL)
+		return (NULL);
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	temp->str = strdup(str);
+	if (temp->str == NULL)
+	{
+		free(temp);
+		return (NULL);
+	}
+	temp->len = _strlen(temp->str);
+	temp->next = *head;
+	*head = temp;
+
+	return (temp);
+}
+
 /**
- * _strlen - Returns the length of a string
- * @s: String to count
- *
- * Return: String length
+ * _strlen - prints the lenght of a string
+ * @s: input character
+ * Return: -strlen
  */
-int _strlen(const char *s)
-{ int c = 0;
-while (*s)
+int _strlen(char *s)
 {
-s++;
-c++; }
-return (c);
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+
+		return (count);
 }
