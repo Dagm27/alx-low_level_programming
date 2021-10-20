0788c2ac015c645efa884a058d88e8d9f12df008
Dagmawi <Dagimanalex27@gmail.com>
2021-10-20 13:35:43 -0700

    main.h and 1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *swap_int - swap value between two integers
+  *@a: integer
+  *@b: integer
+  *
+  *Return: none
+  */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
