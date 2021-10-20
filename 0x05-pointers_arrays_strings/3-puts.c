63a377e3858ae7277e8c4bff3271e87a8ac6f9dd
Dagmawi <Dagimanalex27@gmail.com>
2021-10-20 16:20:02 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *_puts - prints string
+  *@str: string to be printed
+  *
+  *Return:string
+  */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar (*str);
+	}
+	_putchar ('\n');
+}
